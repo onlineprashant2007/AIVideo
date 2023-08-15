b5eece29268a2cacc0b2e911d6838fcd728748dd
onlineprashant2007 <140179891+onlineprashant2007@users.noreply.github.com>
2023-08-15 14:50:59 +0530

    Update aivideo_app.py

--- aivideo_app.py
+++ aivideo_app.py
@@ -1,35 +1,42 @@
 import streamlit as st
-from moviepy.editor import VideoClip
 from gtts import gTTS
+from moviepy.editor import VideoClip, AudioFileClip
+from PIL import Image
 import os
 
 def main():
-    st.title("Audio Animation to Video")
+    st.title("Image Animation with Custom Text")
 
-    uploaded_file = st.file_uploader("Upload an audio file", type=["mp3"])
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png"])
+    custom_text = st.text_input("Enter your custom text:")
+    
+    if uploaded_image and custom_text:
+        # Process the uploaded image
+        image_path = os.path.join("uploads", uploaded_image.name)
+        with open(image_path, "wb") as f:
+            f.write(uploaded_image.getvalue())
 
-    if uploaded_file:
-        audio_path = os.path.join("uploads", uploaded_file.name)
-        with open(audio_path, "wb") as f:
-            f.write(uploaded_file.getvalue())
-
-        text_content = convert_audio_to_text(audio_path)
-        tts = gTTS(text=text_content, lang="en")
+        # Convert custom text to speech
+        tts = gTTS(text=custom_text, lang="en")
         tts_path = os.path.join("uploads", "tts.mp3")
         tts.save(tts_path)
 
-        video_clip = VideoClip(make_frame=lambda t: [tts_path], duration=tts.duration)
+        # Load the image and create an image clip
+        image = Image.open(image_path)
+        image_clip = ImageClip(image, duration=tts.duration)
+
+        # Load the audio and create an audio clip
+        audio_clip = AudioFileClip(tts_path)
+
+        # Combine image and audio clips to create a video clip
+        video_clip = image_clip.set_audio(audio_clip)
+
+        # Write the video to a file
         video_path = os.path.join("uploads", "animated_video.mp4")
         video_clip.write_videofile(video_path, codec="libx264")
 
         st.video(video_path)
 
-def convert_audio_to_text(audio_path):
-    # You would implement this function to convert audio to text
-    # For this example, we'll return a dummy text
-    return "This is a sample text generated from the audio."
-
 if __name__ == "__main__":
     os.makedirs("uploads", exist_ok=True)
     main()
-
